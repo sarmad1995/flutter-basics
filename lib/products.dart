e32ac917e0c9566e6e2c9f15495b1347150ffffc
Sarmad Aijaz <azaabudeen@gmail.com>
2018-10-31 00:18:59 +0530

    added basic desgin

--- lib/products.dart
+++ lib/products.dart
@@ -12,12 +12,39 @@ class Products extends StatelessWidget {
         children: <Widget>[
           Image.asset(products[index]['image']),
           Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Text(products[index]['title'], style: TextStyle(
-              fontSize: 26.0,
-              fontWeight: FontWeight.bold,
-              fontFamily:
-            ),),
+              padding: EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    products[index]['title'],
+                    style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Oswald'),
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.0),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: Text(
+                      '\$${products[index]['price'].toString()}',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              )),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text('Srinagar somewhere '),
           ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
