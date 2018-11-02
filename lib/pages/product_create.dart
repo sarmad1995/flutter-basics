efde0bcdf5e288d90b41c4584749013b0dbfe404
Sarmad Aijaz <azaabudeen@gmail.com>
2018-11-02 23:21:43 +0530

    added form

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -15,11 +15,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _titleValue = '';
   String _descriptionValue = '';
   double _priceValue = 0.0;
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Widget _buildTitleTextField() {
-    return TextField(
+    return TextFormField(
         decoration: InputDecoration(labelText: 'Product Title'),
-        onChanged: (String value) => setState(() => _titleValue = value));
+        onSaved: (String value) => setState(() => _titleValue = value ));
   }
 
   Widget _buildDescriptionTextField() {
@@ -38,6 +38,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   void _submitForm() {
+    _formKey.currentState.save();
     final Map<String, dynamic> product = {
       'title': _titleValue,
       'description': _descriptionValue,
@@ -55,22 +56,25 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double targetPadding = deviceWidth - targetWidth;
     return Container(
         margin: EdgeInsets.all(10.0),
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              color: Theme.of(context).accentColor,
-              textColor: Colors.white,
-              child: Text('Save'),
-              onPressed: _submitForm,
-            )
-          ],
+        child: Form(
+          key: _formKey
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+                child: Text('Save'),
+                onPressed: _submitForm,
+              )
+            ],
+          ),
         ));
   }
 }
